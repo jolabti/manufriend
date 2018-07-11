0d65aaed3a4863ce743d720f322be745035ee783
johan <johan@bareksa.com>
2018-07-11 16:08:05 +0700

    aaa

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,16 +2,16 @@
 
 class Welcome extends CI_Controller {
 
-	// public function __construct() {
-	// 	 parent::__construct();
-	// 	 header("Access-Control-Allow-Origin: *");
-	// 	 header("Content-Type: application/json; charset=UTF-8");
-	// 	 header("Access-Control-Allow-Methods: POST");
-	// 	 header("Access-Control-Max-Age: 3600");
-	// 	 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-	//
-	//
-	// }
+	public function __construct() {
+		 parent::__construct();
+		 header("Access-Control-Allow-Origin: *");
+		 header("Content-Type: application/json; charset=UTF-8");
+		 header("Access-Control-Allow-Methods: POST");
+		 header("Access-Control-Max-Age: 3600");
+		 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
+	}
 
 
 	public function profile()
